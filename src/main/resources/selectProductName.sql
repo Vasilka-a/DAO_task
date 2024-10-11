3cf7cb02efd60d8652e9dee059402809366b61b4
Nastya B <nastybondareva@yandex.ru>
2024-10-11 21:28:58 +0300

    migration project added

--- src/main/resources/selectProductName.sql
+++ src/main/resources/selectProductName.sql
@@ -1,4 +1,4 @@
 select product_name
-from homework_2.orders
-join homework_2.customers c on c.id = homework_2.orders.customer_id
+from orders
+join customers c on c.id = orders.customer_id
 where lower(c.name) = lower(:name);
